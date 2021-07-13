bc0899e2f6dd88f4999a09b8794ad9787145045d
Eduardo Monteiro <dudumonteiro@gmail.com>
2021-07-13 08:40:42 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_cm9uuw4zV', 
+        userPoolClientId: '4spqrr3as9k55hlji0hpbpr9pj', 
+        region: 'us-east' 
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' 
     }
 };
